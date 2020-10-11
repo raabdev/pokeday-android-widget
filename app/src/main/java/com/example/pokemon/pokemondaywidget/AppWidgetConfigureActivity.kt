701468b6a17aeaf6aabd502e46fcb85c5b23bf24
Ivan <ivan.raab21@gmail.com>
2020-10-10 21:02:25 -0300

    se avanzo con las nuevas funcionalidades del widget.

---
+++ app/src/main/java/com/example/pokemon/pokemondaywidget/AppWidgetConfigureActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pokemon.pokemondaywidget
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AppWidgetConfigureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget_configure)
+    }
+
+}
--- app/src/main/java/com/example/pokemon/pokemondaywidget/MyAppWidgetProvider.kt
+++ app/src/main/java/com/example/pokemon/pokemondaywidget/MyAppWidgetProvider.kt
@@ -1,8 +1,10 @@
 package com.example.pokemon.pokemondaywidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.util.Log
 import android.widget.RemoteViews
@@ -25,10 +27,21 @@ class MyAppWidgetProvider : AppWidgetProvider() {
         // Perform this loop procedure for each App Widget that belongs to this provider
         appWidgetIds.forEach { appWidgetId ->
 
+            val pendingIntent: PendingIntent = Intent(context, AppWidgetConfigureActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 0, intent, 0)
+                }
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            val views: RemoteViews = RemoteViews(context.packageName, R.layout.layout_widget_home)
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.layout_widget_home
+            ).apply {
+                setOnClickPendingIntent(R.id.button_configure, pendingIntent)
+            }
+
+
 
 
             getPokemonDay(views, appWidgetManager, appWidgetId, context)
